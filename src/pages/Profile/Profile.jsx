cd232b38ac28acdb261e0e2997573e4c276c54b5
Parth-Bindal27 <parthbindal07@gmail.com>
2026-07-23 00:15:42 +0530

    Profile Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,10 @@
-import ProfileHero from "../../components/profile/ProfileHero/ProfileHero";
-import QuickOverview from "../../components/profile/QuickOverview/QuickOverview";
-import PersonalInfo from "../../components/profile/PersonalInfo/PersonalInfo";
-import AcademicInfo from "../../components/profile/AcademicInfo/AcademicInfo";
-import AboutSection from "../../components/profile/AboutSection/AboutSection";
-import SkillsSection from "../../components/profile/SkillsSection/SkillsSection";
-import SocialLinks from "../../components/profile/SocialLinks/SocialLinks";
+import ProfileHero from "../../components/Profile/ProfileHero/ProfileHero";
+import QuickOverview from "../../components/Profile/QuickOverview/QuickOverview";
+import PersonalInfo from "../../components/Profile/PersonalInfo/PersonalInfo";
+import AcademicInfo from "../../components/Profile/AcademicInfo/AcademicInfo";
+import AboutSection from "../../components/Profile/AboutSection/AboutSection";
+import SkillsSection from "../../components/Profile/SkillsSection/SkillsSection";
+import SocialLinks from "../../components/Profile/SocialLinks/SocialLinks";
 
 export default function Profile() {
   return (
