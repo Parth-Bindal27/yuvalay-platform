4d7175f6b167d09d4ad001ff6d3909e0977bddb7
Parth-Bindal27 <parthbindal07@gmail.com>
2026-07-22 22:08:22 +0530

    Nav Bar 3

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,3 +1,5 @@
+import "./Events.css";
+
 import EventHero from "../../components/Events/EventHero";
 import FeaturedEvent from "../../components/Events/FeaturedEvent";
 import UpcomingEvents from "../../components/Events/UpcomingEvents";
@@ -9,8 +11,7 @@ import EventCTA from "../../components/Events/EventCTA";
 export default function Events(){
 
   return(
-    <>
-
+    <div className="home">
       <EventHero />
       <FeaturedEvent />
       <UpcomingEvents />
